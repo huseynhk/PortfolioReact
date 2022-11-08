18d772d656dcdd3e31f9a5fe3f726693b5807766
huseyn <khuseyn693@gmail.com>
2022-11-08 16:40:34 +0400

    TestiMonials

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import './contact.css';
+import { MdOutlineMail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+
+
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact-options">
+
+          <article className='contact-option'>
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>khuseyn693@gmail.com</h5>
+            <a href="https://mail.google.com/mail/u/0/#inbox" target='_blank'>Send a Message</a>
+          </article>
+
+          <article className='contact-option'>
+            <RiMessengerLine />
+            <h4>Facebook</h4>
+            <h5>Huseyn Huseynzade</h5>
+            <a href="https://www.facebook.com/campaign/landing.php?campaign_id=17872459592&extra_1=s%7Cc%7C613001315951%7Cb%7Cfacebook%20%27%7C&placement=&creative=613001315951&keyword=facebook%20%27&partner_id=googlesem&extra_2=campaignid%3D17872459592%26adgroupid%3D145027591648%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-327195741349%26loc_physical_ms%3D2031%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQiAmaibBhCAARIsAKUlaKRvS-lgJicXv1SthpZLOo4PQmAuoESn_3Zx2pQm4ImbN77yvekOpAAaAp_KEALw_wcB" target='_blank'>Send a Message</a>
+          </article>
+
+          <article className='contact-option'>
+            <BsWhatsapp />
+            <h4>Whatsapp</h4>
+            <h5>055-350-92-92</h5>
+            <a href="https://web.whatsapp.com/" target='_blank'>Send a Message</a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name="name" placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
+
+
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
 import './testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+import AVTR4 from '../../assets/avatar4.jpg';
+
+
+
+const data = [
+  {
+    image: AVTR1,
+    name: 'Ernest Suarez',
+    rewiev: 'Lorem Ipsum is simply dummy text of the printing and typesetting frsjnfklsenflkn idjfiesfilewhjilajfilewjfilhilehifle hfehfiewlhafwihefilehwfile h fihweiofhiowehfioehwiolfhewihoF;IOEWHFO'
+  },
+
+  {
+    image: AVTR2,
+    name: 'Ernest Suarez',
+    rewiev: 'Lorem Ipsum is simply dummy text of the printing and typesetting frsjnfklsenflkn idjfiesfilewhjilajfilewjfilhilehifle hfehfiewlhafwihefilehwfile h fihweiofhiowehfioehwiolfhewihoF;IOEWHFO'
+  },
+
+  {
+    image: AVTR3,
+    name: 'Ernest Suarez',
+    rewiev: 'Lorem Ipsum is simply dummy text of the printing and typesetting frsjnfklsenflkn idjfiesfilewhjilajfilewjfilhilehifle hfehfiewlhafwihefilehwfile h fihweiofhiowehfioehwiolfhewihoF;IOEWHFO'
+  },
+
+  {
+    image: AVTR4,
+    name: 'Ernest Suarez',
+    rewiev: 'Lorem Ipsum is simply dummy text of the printing and typesetting frsjnfklsenflkn idjfiesfilewhjilajfilewjfilhilehifle hfehfiewlhafwihefilehwfile h fihweiofhiowehfioehwiolfhewihoF;IOEWHFO'
+  },
+]
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials_container">
+       {
+        data.map(({image , name , rewiev}, index) => {
+
+        return(
+          <article key={index} className='testimonials'>
+          <div className="client_avatar">
+            <img src={image} alt="" />
+            </div>
+          <div>
+          <h5 className='clients_name'>{name}</h5>
+            <small className='clients_review'>
+               {rewiev}
+            </small>
+          </div>
+          
+        </article>
+        )
+        })
+       }
+       </div>
+      
+    </section>
 
   )
 }
